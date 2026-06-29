3a7a12eeac7db089e85ae3ea3a674b09a3706978
Nick Peng <gibson7780@gmail.com>
2026-06-29 20:15:55 +0800

    28.find-the-index-of-the-first-occurrence-in-a-string

---
+++ leetcode/28.find-the-index-of-the-first-occurrence-in-a-string.go
@@ -0,0 +1,53 @@
+package leetcode
+
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+// Example 1:
+
+// Input: haystack = "sadbutsad", needle = "sad"
+// Output: 0
+// Explanation: "sad" occurs at index 0 and 6.
+// The first occurrence is at index 0, so we return 0.
+// Example 2:
+
+// Input: haystack = "leetcode", needle = "leeto"
+// Output: -1
+// Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+// Constraints:
+
+// 1 <= haystack.length, needle.length <= 104
+// haystack and needle consist of only lowercase English characters.
+
+// 给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串的第一个匹配项的下标（下标从 0 开始）。如果 needle 不是 haystack 的一部分，则返回  -1 。
+
+// 示例 1：
+
+// 输入：haystack = "sadbutsad", needle = "sad"
+// 输出：0
+// 解释："sad" 在下标 0 和 6 处匹配。
+// 第一个匹配项的下标是 0 ，所以返回 0 。
+// 示例 2：
+
+// 输入：haystack = "leetcode", needle = "leeto"
+// 输出：-1
+// 解释："leeto" 没有在 "leetcode" 中出现，所以返回 -1 。
+
+// 提示：
+
+// 1 <= haystack.length, needle.length <= 104
+// haystack 和 needle 仅由小写英文字符组成
+
+// 時間O(n*m) n:外層迴圈 m:字串比較實際會跑回圈逐一比較 但比對單字的話一班遠小於ｎ
+// 空間O(1) 因為slice := haystack[i : len(needle)+i] slice是byte 指向同一個pointer 沒有創新的陣列
+func strStr(haystack string, needle string) int {
+	for i, n := range haystack {
+		if n == rune(needle[0]) && len(needle) <= len(haystack)-i {
+			slice := haystack[i : len(needle)+i]
+			if slice == needle {
+				return i
+			}
+		}
+	}
+	return -1
+}
